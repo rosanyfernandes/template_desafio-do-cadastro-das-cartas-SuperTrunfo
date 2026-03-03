a5be24b3ef224e4507bf62f6404e5a8da61dfa47
rosanyfernandes <124477500+rosanyfernandes@users.noreply.github.com>
2026-03-03 01:01:48 +0000

    NIVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,8 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;
      // Carta 2
     char estado2;
     char codigo2[4];
@@ -22,6 +24,8 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
+    float densidade2;
+    float pibPerCapita2;
 
   // Área para entrada de dados
  printf("=== Cadastro da Carta 1 ===\n");
@@ -71,8 +75,24 @@ int main() {
     printf("Numero de Pontos Turisticos: ");
     scanf("%d", &pontosTuristicos2);
 
+     densidade1 = populacao1 / area1;
+     pibPerCapita1 = pib1 / populacao1;
+     densidade2 = populacao2 / area2;
+     pibPerCapita2 = pib2 / populacao2;
+
   // Área para exibição dos dados da cidade
   
+  printf("\n--- Carta 1 ---\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("Densidade Demografica: %.2f habitantes/km2\n", densidade1);
+    printf("PIB per Capita: %.2f reais/habitante\n", pibPerCapita1);
+    printf("---\n");
   printf("\n--- Carta 2 ---\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
